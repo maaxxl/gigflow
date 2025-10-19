3bb07f05fc47bc10d17a3ffb7e049e798ac4fd72
Maximilian Baur <maximilianbaur@Macmini.fritz.box>
2025-10-19 13:12:10 +0200

    Fix base href placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,153 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'pages/events_page.dart';
+import 'pages/tasks_page.dart';
+import 'pages/settings_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+		const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+		@override
+		Widget build(BuildContext context) {
+			// iOS26-like color palette
+			const Color primary = Color(0xFF0A84FF);
+			const Color background = Color(0xFFF6F6F7);
+			const Color surface = Colors.white;
+
+			final theme = ThemeData(
+				brightness: Brightness.light,
+				primaryColor: primary,
+				scaffoldBackgroundColor: background,
+				canvasColor: surface,
+				useMaterial3: true,
+				textTheme: const TextTheme(
+					headlineLarge: TextStyle(fontSize: 34, fontWeight: FontWeight.w700, color: Colors.black),
+					titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black87),
+					bodyMedium: TextStyle(fontSize: 16, color: Colors.black87),
+				),
+				cardTheme: const CardTheme(shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16)))),
+			);
+
+			return MaterialApp(
+				title: 'Gigflow',
+				theme: theme,
+				home: const HomeShell(),
+			);
+		}
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeShell extends StatefulWidget {
+	const HomeShell({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+	@override
+	State<HomeShell> createState() => _HomeShellState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeShellState extends State<HomeShell> {
+	int _currentIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+	final List<Widget> _pages = const [
+		EventsPage(),
+		TasksPage(),
+		SettingsPage(),
+	];
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+	void _onTap(int index) {
+		setState(() => _currentIndex = index);
+	}
+
+		@override
+		Widget build(BuildContext context) {
+				return Scaffold(
+					body: IndexedStack(
+						index: _currentIndex,
+						children: _pages,
+					),
+					bottomNavigationBar: SafeArea(
+						minimum: const EdgeInsets.only(bottom: 8, left: 12, right: 12),
+						child: _buildIosTabBar(context),
+					),
+				);
+		}
+
+			Widget _buildIosTabBar(BuildContext context) {
+				// saturated iOS-like colors
+					const Color eventsColor = Color(0xFF0A84FF); // blue
+					const Color tasksColor = Color(0xFF34C759); // green (phone app)
+					const Color settingsColor = Color(0xFFFF9F0A); // orange
+
+					// make the bar lighter/brighter than the page background
+					final barColor = const Color.fromRGBO(255, 255, 255, 0.96);
+
+					return ClipRRect(
+						borderRadius: BorderRadius.circular(30),
+						child: BackdropFilter(
+							filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
+							child: Container(
+								padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 14),
+								decoration: BoxDecoration(
+									color: barColor,
+									borderRadius: BorderRadius.circular(30),
+									border: Border.all(color: Colors.white.withAlpha((0.85 * 255).round())),
+									boxShadow: [
+										BoxShadow(color: Colors.black.withAlpha((0.08 * 255).round()), blurRadius: 18, offset: const Offset(0, 8)),
+									],
+								),
+								child: Row(
+									children: List.generate(3, (i) {
+										final active = i == _currentIndex;
+										IconData icon;
+										Color activeColor;
+										if (i == 0) {
+											icon = CupertinoIcons.calendar;
+											activeColor = eventsColor;
+										} else if (i == 1) {
+											icon = CupertinoIcons.check_mark;
+											activeColor = tasksColor;
+										} else {
+											icon = CupertinoIcons.settings;
+											activeColor = settingsColor;
+										}
+
+
+										return Expanded(
+											child: GestureDetector(
+												behavior: HitTestBehavior.opaque,
+												onTap: () => _onTap(i),
+												child: Column(
+													mainAxisSize: MainAxisSize.min,
+													children: [
+																				AnimatedContainer(
+																					duration: const Duration(milliseconds: 220),
+																					curve: Curves.easeOutCubic,
+																					padding: const EdgeInsets.all(10),
+																					decoration: BoxDecoration(
+																						color: active ? activeColor : Colors.grey.shade100,
+																						borderRadius: BorderRadius.circular(14),
+																						border: active ? null : Border.all(color: Colors.grey.shade300, width: 0.8),
+																						// subtle inner shadow when active
+																						boxShadow: active
+																								? [BoxShadow(color: activeColor.withAlpha((0.18 * 255).round()), blurRadius: 8, offset: const Offset(0, 4))]
+																								: null,
+																					),
+																					child: Icon(
+																						icon,
+																						size: 30,
+																						color: active ? Colors.white : Colors.grey.shade700,
+																					),
+																				),
+													],
+												),
+											),
+										);
+									}),
+								),
+							),
+						),
+					);
+			}
 }
+
---
+++ lib/pages/tasks_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class TasksPage extends StatelessWidget {
+  static const String title = 'Tasks';
+
+  const TasksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          backgroundColor: theme.canvasColor,
+          elevation: 0,
+          expandedHeight: 140,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: const EdgeInsetsDirectional.only(start: 16, bottom: 16, end: 16),
+            title: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(title, style: theme.textTheme.headlineLarge),
+            ),
+            collapseMode: CollapseMode.pin,
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          ],
+        ),
+        SliverFillRemaining(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('InWork', style: TextStyle(fontSize: 24)),
+                SizedBox(height: 8),
+                Text(title, style: TextStyle(fontSize: 18)),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
